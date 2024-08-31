aa94107a7088b525104d04de29b3009634de43e8
DSSTUZ <tursunovuralbek225@gmail.com>
2024-09-01 00:37:55 +0500

    update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "uralbe.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    var x = xmlDoc.getElementsByTagName("year");
+    document.getElementById("demo").innerHTML =
+    x[2].childNodes[0].nodeValue;
+}
