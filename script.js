9602f288a4b7844e105ab42feef92a85d85e98e7
DSSTUZ <tursunovuralbek225@gmail.com>
2024-09-01 01:24:45 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("demo").innerHTML = "Bugun mening tug'ilgan kunim ☺️.";
 
-document.getElementById("demo2").innerHTML = "Tabriklayman";
+document.getElementById("demo2").innerHTML = "Tabriklayman!";
