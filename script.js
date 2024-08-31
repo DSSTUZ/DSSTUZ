1b8efb93c91b0bcc94012944e8f5fa8f71b04a4a
DSSTUZ <tursunovuralbek225@gmail.com>
2024-09-01 00:05:32 +0500

    update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML =
-      this.responseText;
+     document.getElementById("demo").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "https://uralbekuzbot.netlify.app/ajax.txt", true);
+  xhttp.open("GET", "https://uralbekuzbotajax.txt", true);
   xhttp.send();
 }
