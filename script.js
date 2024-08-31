20eaa704d376b94aff20214abbf4680015ffe846
DSSTUZ <tursunovuralbek225@gmail.com>
2024-09-01 01:21:17 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("demo").innerHTML = "Bugun mening tug'ilgan kunim ☺️";
 
-document.getElementById("demo2").innerHTML = "Tabrik va maqtov shart ";
+document.getElementById("demo2").innerHTML = "Tabrik va maqtov shart emas!";
