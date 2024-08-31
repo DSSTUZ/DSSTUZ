590938de06d5537fc7e7ebd27e11b1d27d351f6b
DSSTUZ <tursunovuralbek225@gmail.com>
2024-09-01 01:04:55 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-    }
-};
-xhttp.open("GET", "https://uralbekuzbot.netlify.app/uralbekuz.xml", true);
-xhttp.send();
-
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    var x = xmlDoc.getElementsByTagName("year");
-    document.getElementById("demo6").innerHTML =
-    x[2].childNodes[0].nodeValue;
-}
+document.getElementById("demo").innerHTML = "Hello World! Buguning uchun";
